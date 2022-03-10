b5d42620549274e57a60ceac0852bb258fefd6c8
Luka <vujnovacluka89@gmail.com>
2022-03-10 16:39:12 +0100

    TabBarViewController added

---
+++ SofaScorePrvoPredavanje/SofaScorePrvoPredavanje/ProfileVC.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileVC.swift
+//  SofaScorePrvoPredavanje
+//
+//  Created by Luka Vujnovac on 10.03.2022..
+//
+import UIKit
+
+class ProfileVC: ViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
---
+++ SofaScorePrvoPredavanje/SofaScorePrvoPredavanje/SearchVC.swift
@@ -0,0 +1,16 @@
+//
+//  SearchVC.swift
+//  SofaScorePrvoPredavanje
+//
+//  Created by Luka Vujnovac on 10.03.2022..
+//
+
+import UIKit
+
+class SearchVC: ViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
