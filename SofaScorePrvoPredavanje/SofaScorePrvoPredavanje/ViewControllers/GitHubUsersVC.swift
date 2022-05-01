98cca96eaae62311ee7144942512709b471858e4
Luka <vujnovacluka89@gmail.com>
2022-05-01 18:47:20 +0200

    UI improvments

--- SofaScorePrvoPredavanje/SofaScorePrvoPredavanje/ViewControllers/GitHubUsersVC.swift
+++ SofaScorePrvoPredavanje/SofaScorePrvoPredavanje/ViewControllers/GitHubUsersVC.swift
@@ -6,13 +6,13 @@
 //
 
 import UIKit
-import SwiftUI
 
 class GitHubUsersVC: UIViewController, UISearchBarDelegate {
-    
     private var username: String = ""
-    
     private let searchBar = UISearchBar()
+    private let button = UIButton()
+    
+    var followersx: [Follower] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,11 +21,12 @@ class GitHubUsersVC: UIViewController, UISearchBarDelegate {
         view.backgroundColor = .systemBackground
         
         addViews()
+        configureButton()
         setupConstraints()
     }
     
-    func addViews() {
-        view.addSubviews(searchBar)
+    private func addViews() {
+        view.addSubviews(searchBar, button)
     }
     
     override func viewDidLayoutSubviews() {
@@ -38,20 +39,48 @@ class GitHubUsersVC: UIViewController, UISearchBarDelegate {
         NetworkManager.shared.getFollowers(for: text, page: 1) { [weak self] result in
             switch result {
                 case .success(let followers):
-                    print(followers[0].url)
+//                    print(followers)
+                    if !followers.isEmpty {
+                        self?.followersx = followers
+                        print(self?.followersx[0].url)
+                    }else {
+                        print("no followers")
+                    }
+                    
                 case .failure(let error):
                     print(error)
             }
         }
     }
     
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        if let text = searchBar.text {
-            fetchFollowers(text: text)
-        }
+    @objc private func didTapButton() {
+        let navVC = UINavigationController(rootViewController: UserDetailVC())
+        fetchFollowers(text: searchBar.text ?? "")
+        
+        //collection view svih ljudi da ga mos kasnije reuse za followere
+        
+        present(navVC, animated: true)
     }
     
-    func setupConstraints() {
-        
+//    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+//        if let text = searchBar.text {
+//            fetchFollowers(text: text)
+//        }
+//    }
+    
+    private func configureButton() {
+        button.setTitle("Find User", for: .normal)
+        button.layer.cornerRadius = 20
+        button.backgroundColor = .systemPurple
+        button.setTitleColor(.label, for: .normal)
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+    }
+    
+    private func setupConstraints() {
+        button.snp.makeConstraints{
+            $0.centerX.centerY.equalToSuperview()
+            $0.width.equalTo(200)
+            $0.height.equalTo(52)
+        }
     }
 }
--- SofaScorePrvoPredavanje/SofaScorePrvoPredavanje/ViewControllers/UserDetailVC.swift
+++ SofaScorePrvoPredavanje/SofaScorePrvoPredavanje/ViewControllers/UserDetailVC.swift
@@ -6,14 +6,18 @@
 //
 
 import UIKit
-import SwiftUI
+import SafariServices
 
 class UserDetailVC: UIViewController {
     private var avatarView = GitHubAvatarImageView(frame: CGRect(x: 0, y: 0, width: 80, height: 80))
     private var usernameView = UILabel()
     private var bioView = UILabel()
-    private var profileStackView = UIStackView()
-    private var followersStackView = UIStackView()
+    private var reposView = UILabel()
+    private var gistsView = UILabel()
+    private var profileURLButton = UIButton()
+    private var followersView = UILabel()
+    private var followingView = UILabel()
+    private var followersButtonView = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,111 +26,86 @@ class UserDetailVC: UIViewController {
         addViews()
         styleViews()
         setupConstraints()
-        configureProfileContainerView()
-        configureFollowersContainerView()
     }
     
     private func addViews() {
-        view.addSubviews(avatarView, usernameView, bioView, profileStackView, followersStackView)
+        view.addSubviews(avatarView, usernameView, bioView, reposView, gistsView, profileURLButton, followingView, followersView, followersButtonView) //, profileStackView, followersStackView)
     }
-    
-    private func configureProfileContainerView() {
-        let reposView = UILabel()
-        let gistsView = UILabel()
-        let buttonView = UIButton()
-        
-        profileStackView.addSubviews(gistsView, reposView, buttonView)
-        
-        reposView.text = "Public Repos: 50"
-        gistsView.text = "Public Gists: 0"
-        buttonView.setTitle("View Profile", for: .normal)
-        buttonView.setTitleColor(.label, for: .normal)
-        buttonView.backgroundColor = .systemPurple
-        buttonView.layer.cornerRadius = 10
-        
-        reposView.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
-        gistsView.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
-        
-        reposView.snp.makeConstraints{
-            $0.leading.equalToSuperview().offset(18)
-            $0.top.equalToSuperview().offset(18)
-        }
-        
-        gistsView.snp.makeConstraints{
-            $0.trailing.equalToSuperview().offset(-18)
-            $0.top.equalTo(reposView)
-        }
-        
-        buttonView.snp.makeConstraints { 
-            $0.top.equalTo(gistsView.snp.bottom).offset(18)
-            $0.width.equalToSuperview().offset(-40)
-            $0.height.equalTo(40)
-            $0.centerX.equalToSuperview()
-        }
-        
-    }
-    
-    private func configureFollowersContainerView() {
-        let followersView = UILabel()
-        let followingView = UILabel()
-        let buttonView = UIButton()
-        
-        followersStackView.addSubviews(followingView, followersView, buttonView)
-        
-        followersView.text = "Followers: 0"
-        followingView.text = "Following: 0"
-        buttonView.setTitle("Show Followers", for: .normal)
-        buttonView.setTitleColor(.label, for: .normal)
-        buttonView.backgroundColor = .systemBlue
-        buttonView.layer.cornerRadius = 10
-        
-        followersView.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
-        followingView.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
-        
-        followersView.snp.makeConstraints{
-            $0.leading.equalToSuperview().offset(18)
-            $0.top.equalToSuperview().offset(18)
-        }
-        
-        followingView.snp.makeConstraints{
-            $0.trailing.equalToSuperview().offset(-18)
-            $0.top.equalTo(followersView)
-        }
-        
-        buttonView.snp.makeConstraints { 
-            $0.top.equalTo(followingView.snp.bottom).offset(18)
-            $0.width.equalToSuperview().offset(-40)
-            $0.height.equalTo(40)
-            $0.centerX.equalToSuperview()
-        }
-        
-    }
-    
     
     private func styleViews() {
-        avatarView.image = UIImage(systemName: "person")
-        avatarView.layer.cornerRadius = 10
-        avatarView.layer.shadowRadius = 5
-        
-        usernameView.text = "lukavujnovac"
-        usernameView.font = UIFont.boldSystemFont(ofSize: 20)
-        
-        bioView.text = "software dev"
-        bioView.font = UIFont.systemFont(ofSize: 17, weight: .thin)
-        
-        profileStackView.axis = .vertical
-        profileStackView.spacing = 18
-        
-        followersStackView.axis = .vertical
+        styleAvatarView()
+        styleUsernameView()
+        styleBioView()
+        styleGistsView()
+        styleReposView()
+        styleProfileButton()
     }
     
     private func setupConstraints() {
         setupAvatarViewConstraints()
         setupUsernameViewConstraints()
         setupBioViewConstraints()
-        setupProfileContainerViewConstraints()
-        setupFollowersContainerViewConstraints()
+        setupReposViewConstraints()
+        setupGistsViewCOnstraints()
+        setupProfileButtonConstraints()
+        //setupProfileContainerViewConstraints()
+        //setupFollowersContainerViewConstraints()
     }
+    
+    @objc func handleProfileURL() {
+        guard let url = URL(string: "https://github.com/lukavujnovac") else {return}
+        let svc = SFSafariViewController(url: url)
+        self.present(svc, animated: true)
+    }
+}
+
+
+
+private extension UserDetailVC {
+    func styleAvatarView() {
+        avatarView.image = UIImage(systemName: "person")
+        avatarView.layer.cornerRadius = 10
+        avatarView.layer.shadowRadius = 5
+    }
+    
+    func styleUsernameView() {
+        usernameView.text = "lukavujnovac"
+        usernameView.font = UIFont.boldSystemFont(ofSize: 20)
+    }
+    
+    func styleBioView() {
+        bioView.text = "software dev"
+        bioView.font = UIFont.systemFont(ofSize: 17, weight: .thin)
+    }
+    
+    func styleReposView() {
+        reposView.text = "Public Repos: 10"
+        reposView.font = UIFont.systemFont(ofSize: 17, weight: .semibold)
+    }
+    
+    func styleGistsView() {
+        gistsView.text = "Public Gists: 0"
+        gistsView.font = UIFont.systemFont(ofSize: 17, weight: .semibold)
+    }
+    
+    func styleProfileButton() {
+        profileURLButton.setTitle("Visit GitHub Profile", for: .normal)
+        profileURLButton.setTitleColor(.label, for: .normal)
+        profileURLButton.backgroundColor = .systemPurple
+        profileURLButton.layer.cornerRadius = 10
+                    
+        profileURLButton.addTarget(self, action: #selector(handleProfileURL), for: .touchUpInside)
+    }
+    
+//    func styleProfileStackView() {
+//        profileStackView.axis = .vertical
+//        profileStackView.spacing = 18
+//    }
+//    
+//    func styleFollowersStackView() {
+//        followersStackView.axis = .vertical
+//        profileStackView.spacing = 18
+//    }
 }
 
 private extension UserDetailVC {
@@ -152,17 +131,111 @@ private extension UserDetailVC {
         }
     }
     
-    func setupProfileContainerViewConstraints() {
-        profileStackView.snp.makeConstraints { 
-            $0.top.equalTo(bioView.snp.bottom).offset(50)
-            $0.leading.trailing.equalToSuperview().inset(18)
+    func setupReposViewConstraints() {
+        reposView.snp.makeConstraints { 
+            $0.leading.equalToSuperview().offset(36)
+            $0.top.equalTo(bioView.snp.bottom).offset(24)
         }
     }
     
-    func setupFollowersContainerViewConstraints() {
-        followersStackView.snp.makeConstraints { 
-            $0.top.equalTo(profileStackView.snp.bottom).offset(150)
-            $0.leading.trailing.equalToSuperview().inset(18)
+    func setupGistsViewCOnstraints() {
+        gistsView.snp.makeConstraints { 
+            $0.trailing.equalToSuperview().offset(-36)
+            $0.top.equalTo(bioView.snp.bottom).offset(24)
+        }
+    }
+    
+    func setupProfileButtonConstraints() {
+        profileURLButton.snp.makeConstraints { 
+            $0.leading.trailing.equalToSuperview().inset(36)
+            $0.top.equalTo(gistsView.snp.bottom).offset(18)
+            $0.width.equalTo(200)
+            $0.height.equalTo(52)
         }
     }
 }
+
+//private extension UserDetailVC {
+//    func configureProfileStackView() {
+        
+//        let urlButtonView: UIButton = {
+//            let button = UIButton(type: .system)
+//            button.setTitle("Visit GitHub Profile", for: .normal)
+//            button.setTitleColor(.label, for: .normal)
+//            button.backgroundColor = .systemPurple
+//            button.layer.cornerRadius = 10
+//            
+//            button.addTarget(self, action: #selector(handleProfileURL), for: .touchUpInside)
+//            
+//            return button
+//        }()
+//        profileStackView.addSubviews(gistsView, reposView, urlButtonView)
+//        
+//        reposView.text = "Public Repos: 50"
+//        gistsView.text = "Public Gists: 0"
+//        
+//        reposView.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
+//        gistsView.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
+//        
+//        reposView.snp.makeConstraints{
+//            $0.leading.equalToSuperview().offset(18)
+//            $0.top.equalToSuperview().offset(18)
+//        }
+//        
+//        gistsView.snp.makeConstraints{
+//            $0.trailing.equalToSuperview().offset(-18)
+//            $0.top.equalTo(reposView)
+//        }
+//        
+//        urlButtonView.snp.makeConstraints { 
+//            $0.top.equalTo(gistsView.snp.bottom).offset(18)
+//            $0.width.equalToSuperview().offset(-40)
+//            $0.height.equalTo(40)
+//            $0.centerX.equalToSuperview()
+//        }
+//        
+//    }
+//    
+    
+//    
+//    func configureFollowersStackView() {
+        
+//        
+//        followersStackView.addSubviews(followingView, followersView, buttonView)
+//        
+//        followersView.text = "Followers: 0"
+//        followingView.text = "Following: 0"
+//        buttonView.setTitle("Show Followers", for: .normal)
+//        buttonView.setTitleColor(.label, for: .normal)
+//        buttonView.backgroundColor = .systemBlue
+//        buttonView.layer.cornerRadius = 10
+//        buttonView.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+//        
+//        followersView.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
+//        followingView.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
+//        
+//        followersView.snp.makeConstraints{
+//            $0.leading.equalToSuperview().offset(18)
+//            $0.top.equalToSuperview().offset(18)
+//        }
+//        
+//        followingView.snp.makeConstraints{
+//            $0.trailing.equalToSuperview().offset(-18)
+//            $0.top.equalTo(followersView)
+//        }
+//        
+//        buttonView.snp.makeConstraints { 
+//            $0.top.equalTo(followingView.snp.bottom).offset(18)
+//            $0.width.equalToSuperview().offset(-40)
+//            $0.height.equalTo(40)
+//            $0.centerX.equalToSuperview()
+//        }
+//    }
+//    @objc private func didTapButton() {
+//        let rootVC = UIViewController()
+//        rootVC.title = "welcome"
+//        let navVC = UINavigationController(rootViewController: rootVC)
+//        
+//        present(navVC, animated: true)
+//    }
+//}
